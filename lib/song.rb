27c52496ff0799ae34d22dceeb6365700e7ce805
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-13 06:19:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,10 +5,10 @@ class Song
    @name = name 
  end 
  
- def artist_name=(name)
-   new_artist = Artist.find_or_create_by_name(name)
-   new_artist.add_song(self)
- end 
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
 
   def self.new_by_filename(file_name)
     song = file_name.split(" - ")[1]
