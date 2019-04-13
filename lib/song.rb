b715773760d1736720cc8a331e64dab36960d995
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-13 06:17:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,14 +10,13 @@ class Song
    new_artist.add_song(self)
  end 
 
- def self.new_by_filename(filename) 
-    title_artist = filename.split(" - ")
-    new_song = self.new 
-    new_song.name = title_artist[1].chomp(".mp3")
-    new_song.artist = title_artist[0]
+  def self.new_by_filename(file_name)
+    song = file_name.split(" - ")[1]
+    artist = file_name.split(" - ")[0]
+    new_song = self.new(song)
+    new_song.artist_name = artist
     new_song
-    
-  end 
+  end
  
     
  
