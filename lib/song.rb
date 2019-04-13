23988ac511eec36f3d9bd00e3290c3808be166cf
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-13 06:12:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,7 @@ class Song
  end 
  
  def artist_name=(name)
-   self.artist_name = artist_name
+   self.artist_name = Artist.find_or_create_by_name(name)
  end 
 
  def self.new_by_filename(filename) 
