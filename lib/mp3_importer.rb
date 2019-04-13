8a7466ca1d1fab69cedd01ab20ecfe244a753dd5
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-13 05:28:42 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -8,18 +8,18 @@ class MP3Importer
   
   def files
     files_array = Dir.entries(@path)
-    mp3_array = []
+    @mp3_array = []
     files_array.each do |file|
       if file.include?(".mp3") 
-        mp3_array << file 
+        @mp3_array << file 
       end 
     end 
-    mp3_array
+    @mp3_array
   end 
   
   def import 
     mp3_array = self.files 
-    mp3_array.each {|file| Song.new_by_filename(file) }
+    @mp3_array.each {|file| Song.new_by_filename(file) }
   end 
     
 end 
